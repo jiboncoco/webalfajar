c4f3ef04c8cc1e0943769f5b8326b7fa744b3a5d
rzdannisa <rizda25.9c@gmail.com>
2016-04-15 06:53:39 +0700

    update login staff

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -15,24 +15,21 @@ class LoginController extends Controller
 		$akses_code     = $_POST['akses_code'];
 		$akses_username = $_POST['akses_username'];
 		$akses_password = $_POST['akses_password'];
-		
-			$akses_statuss = \DB::table('akses_type')->where('akses_type_status', '=', 'true')->count();
-			
-			if (!empty($akses_statuss)) { 
 
 				$akses_codee = \DB::table('akses_log')->where('akses_log_code', 'like', $akses_code)->count();
 				
 				if (!empty($akses_codee)) {
-					
-					$cekdatevalid = \DB::table('akses')->join('akses_log', function($join)
-					{
-						$join->on('akses.akses_id', '=', 'akses_log.akses_log_id');
-					})->get();
+
+					$cekdatevalid = \DB::table('akses_log')->where('akses_log_code', 'like', $akses_code)->get();
 					$tanggal = \DateTime::createFromFormat('Y-m-d H:i:s', $cekdatevalid[0]->akses_log_timestamp)->format('d');
+					$cekdatevalidd = \DB::table('akses_log')->where('akses_log_datevalid',$tanggal)->count();
 		
-					if (!empty($cekdatevalid)) {
-						
-						$akses_status_dataa = \DB::table('akses')->where('akses_code', 'like', $akses_code, 'AND', 'akses.akses_status_data', '=', 'active')->count();
+					if ($cekdatevalidd == 1) {
+
+						$akses_status_dataa = \DB::table('akses_type')->where([
+							['akses_type.akses_type_name', 'like', 'staff'],
+							['akses_type.akses_type_status', 'like', 'true']
+							])->count();
 						
 						if (!empty($akses_status_dataa)) {
 							
@@ -49,35 +46,45 @@ class LoginController extends Controller
 										['akses_password', 'like', $akses_password], 
 										['akses_type', 'like', 'staff']
 									])->count();
-									if (!empty($akses_typee)) {
+										if (!empty($akses_typee)) {
+
+											$akses_statuss = \DB::table('akses')->where([
+											['akses_code', 'like', $akses_code],
+											['akses_type', 'like', 'staff'],
+											['akses_username', 'like', $akses_username],
+											['akses_password', 'like', $akses_password], 
+											['akses_status_data', 'like', 'active']
+											])->count();
+							
+										if (!empty($akses_statuss)) { 
 										$_SESSION['logged_in'] = 1;
 									return redirect('admin');
 								} else {
-									$_SESSION['error_msg'] = "Account not have Access Type";
+									$_SESSION['error_msg'] = "Account has been Disable";
 									return redirect('login');
 								}
 							} else {
-								$_SESSION['error_msg'] = "Password you’ve entered doesn’t match any account";
+								$_SESSION['error_msg'] = "Account not have Access Type";
 								return redirect('login');
 							}
 						} else {
-							$_SESSION['error_msg'] = "Username you’ve entered doesn’t match any account";
+							$_SESSION['error_msg'] = "Password you’ve entered doesn’t match any account";
 							return redirect('login');
 						}
 					} else {
-						$_SESSION['error_msg'] = "Account has been Disable";
+						$_SESSION['error_msg'] = "Username you’ve entered doesn’t match any account";
 						return redirect('login');
 					}
 				} else {
-					$_SESSION['error_msg'] = "Access Code Has been Expired";
+					$_SESSION['error_msg'] = "Access Type hasbeen Disable";
 					return redirect('login');
 				}
 			} else {
-				$_SESSION['error_msg'] = "Access Code Not Registered";
+				$_SESSION['error_msg'] = "Access Code Has been Expired";
 				return redirect('login');
 			}
 		} else {
-			$_SESSION['error_msg'] = "Access Type hasbeen Disable";
+			$_SESSION['error_msg'] = "Access Code Not Registered";
 			return redirect('login');
 		}
 	}
