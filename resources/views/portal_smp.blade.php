c1037cb20e45e82d8bd79e9e8f62f2b8bacd79ab
rzdannisa <rizda25.9c@gmail.com>
2016-05-16 06:20:06 +0700

    update url

--- resources/views/portal_smp.blade.php
+++ resources/views/portal_smp.blade.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link id="callCss" rel="stylesheet" href="/css/bootstrap.min.css" type="text/css" media="screen" charset="utf-8" />
+    <link id="callCss" rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}" type="text/css" media="screen" charset="utf-8" />
     <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="{{ url('font-awesome/css/font-awesome.min.css') }}">
-    <link id="callCss"rel="stylesheet" href="/css/style.css" type="text/css" media="screen" charset="utf-8" />
+    <link id="callCss"rel="stylesheet" href="{{ url('css/style.css') }}" type="text/css" media="screen" charset="utf-8" />
 
 </head>
 <body>
@@ -620,11 +620,11 @@
 <a href="#" class="go-top" ><i class="glyphicon glyphicon-arrow-up"></i></a>
 
 
-<script src="js/jquery-1.9.1.min.js"></script>
-<script src="js/bootstrap.min.js" type="text/javascript"></script>
-<script src="js/jquery.scrollTo-1.4.3.1-min.js" type="text/javascript"></script>
-<script src="js/jquery.easing-1.3.min.js"></script>
-<script src="js/default.js"></script>
+<script src="{{ url('js/jquery-1.9.1.min.js') }}"></script>
+<script src="{{ url('js/bootstrap.min.js') }}" type="text/javascript"></script>
+<script src="{{ url('js/jquery.scrollTo-1.4.3.1-min.js') }}" type="text/javascript"></script>
+<script src="{{ url('js/jquery.easing-1.3.min.js') }}"></script>
+<script src="{{ url('js/default.js') }}"></script>
 
 <script type="text/javascript">
 
